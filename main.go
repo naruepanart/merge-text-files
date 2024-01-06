10484fe0e0984beb7465269c3d22aa1202119c2b
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-01-06 19:14:34 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,45 +2,61 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
-	"io"
+	"sort"
+	"strconv"
 )
 
-func mergeTextFiles(outputFileName string) error {
+func mergeTextFiles(outputFile string) error {
 	files, err := filepath.Glob("*.txt")
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to retrieve files: %v", err)
 	}
 
-	outputFile, err := os.Create(outputFileName)
+	sortFilesByNumber(files)
+
+	outFile, err := os.Create(outputFile)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create output file: %v", err)
 	}
-	defer outputFile.Close()
+	defer outFile.Close()
 
 	for _, file := range files {
-		inputFile, err := os.Open(file)
-		if err != nil {
-			return err
-		}
-		defer inputFile.Close()
-
-		_, err = io.Copy(outputFile, inputFile)
-		if err != nil {
+		fmt.Println("Processing:", file)
+		if err := mergeFiles(outFile, file); err != nil {
 			return err
 		}
 	}
 
-	fmt.Println("Merge successful Output file:", outputFileName)
+	fmt.Println("Merge successful. Output file:", outputFile)
 	return nil
 }
 
-func main() {
-	outputFileName := "merged.txt"
+func sortFilesByNumber(files []string) {
+	sort.Slice(files, func(i, j int) bool {
+		numI, _ := strconv.Atoi(filepath.Base(files[i][:len(files[i])-4]))
+		numJ, _ := strconv.Atoi(filepath.Base(files[j][:len(files[j])-4]))
+		return numI < numJ
+	})
+}
 
-	err := mergeTextFiles(outputFileName)
+func mergeFiles(output io.Writer, filePath string) error {
+	file, err := os.Open(filePath)
 	if err != nil {
+		return fmt.Errorf("failed to open file %s: %v", filePath, err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(output, file)
+	return err
+}
+
+func main() {
+	outputFile := "merged.txt"
+
+	if err := mergeTextFiles(outputFile); err != nil {
 		fmt.Println("Error:", err)
 	}
 }
